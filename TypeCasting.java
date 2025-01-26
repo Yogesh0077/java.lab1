a8046f0eef142e54edb5d50d6b0d0b0d4015b0e2
Yogesh0077 <cresthayoogeeshh@gmail.com>
2025-01-26 08:51:56 +0545

    Add files via upload
    This report is for typecasting in java.

---
+++ TypeCasting.java
@@ -0,0 +1,31 @@
+package Lab1;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        // Widening casting
+        /*
+        converting a smaller type to larger type size
+         */
+        int myInt = 9;
+        System.out.println("Integer value: "+ myInt);
+        double myDouble = myInt; // Automatic casting int to double
+        System.out.println("Casting int to double : " + myDouble);
+
+        // Narrowing casting
+        /*
+        converting a larger type to smaller type. Done manually by placing the type in parentheses () in front of the value
+         */
+        double mySecondDouble = 9.78f;
+        System.out.println("Double value: " + mySecondDouble);
+        int mySecondInt = (int) mySecondDouble; // manual casting : double to int
+        System.out.println("Casting double to int: " + mySecondInt);
+
+
+        // Another example
+        int maxScore =500;
+        int userScore =423;
+
+        float percentage = ((float) userScore /maxScore)*100.0f;
+        System.out.println("User percentage is : " + percentage);
+    }
+}
