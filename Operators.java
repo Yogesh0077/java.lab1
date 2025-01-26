24b1b30ffdd4b5c45328266bf70e16b9190c8c90
Yogesh0077 <cresthayoogeeshh@gmail.com>
2025-01-26 08:50:54 +0545

    Add files via upload
    this report is for operator in java.

---
+++ Operators.java
@@ -0,0 +1,19 @@
+package Lab1;
+
+public class Operators {
+    public static void main(String[] args) {
+        //Operstors are used to perform operations on variables and values
+        //Arithmetic
+        int sumValue = 5 + 10 ;
+        System.out.println("Sum: "+sumValue);
+
+        //Assignment
+        int assign =10;
+        assign += 5;
+        System.out.println("Sum using Add assignment: "+assign);
+
+        //Comparison
+        int x = 5, y=10;
+        System.out.println("Check x(5) is greater than y(10): "+(x>y));
+    }
+}
